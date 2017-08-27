058e4a54a1c89bd9afda877541800cd9f1174149
lanre01 <olanre12waju@gmail.com>
2017-08-27 22:17:26 +0100

    initial commit

---
+++ js/helper.js
@@ -0,0 +1,5 @@
+var HTMLheaderName = '<h1 id="name">%data%</h1>';
+var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLslack = '<li class="flex-item"><span class="orange-text">slack></span><span class="white-text">%data%</span></li>';
+var HTMLBioMsg = '<span class="bio-message">%data%</span>';
